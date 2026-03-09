c0673b8ab44ef59e0f39f38deb12f18430a596d7
Gaurav <gauravbhat7121@icloud.com>
2026-03-09 14:42:14 +0530

    extreamly cleaned

--- RUST2026/RustAdvance/Collections/vector/src/main.rs
+++ RUST2026/RustAdvance/Collections/vector/src/main.rs
@@ -10,7 +10,4 @@ fn main() {
     reading::readingElements();
     iterating::iteratingVector();
     enum_vec::accountsVector();
-    // vectors are impl by generics 
-    // Vec<T> is generic , T = any type of data
-
 }
