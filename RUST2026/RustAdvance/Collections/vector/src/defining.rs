344949e1a79d415343f754e95f4401e11e6c9774
Gaurav <gauravbhat7121@icloud.com>
2026-03-09 11:13:04 +0530

    update to public

--- RUST2026/RustAdvance/Collections/vector/src/defining.rs
+++ RUST2026/RustAdvance/Collections/vector/src/defining.rs
@@ -1,4 +1,4 @@
-fn defining() {
+pub fn defining() {
     let v: Vec<i32> = Vec::new(); // for this we need to give data type 
     // let v1 = Vec::new(1.23,12.23);  this will couse error 
     let w = vec![1.2,1.5]; // this will automatic verify the data type 
@@ -11,7 +11,5 @@ fn defining() {
     // All this happening coz we need to give an hint to compiler at ccompile time 
     // that how much storage will one element will need 
     // and that will happen by giving hint that which kind of data we are going 
-    // to store in it
-            
-    
+    // to store in it 
 }
