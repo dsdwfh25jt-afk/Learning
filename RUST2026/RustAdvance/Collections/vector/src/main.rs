9bc5f9e673e48f6df39fb3e7a233517c1738484d
Gaurav <gauravbhat7121@icloud.com>
2026-03-09 14:39:03 +0530

    cleaned

--- RUST2026/RustAdvance/Collections/vector/src/main.rs
+++ RUST2026/RustAdvance/Collections/vector/src/main.rs
@@ -1,22 +1,16 @@
-// use crate::vector::reading::{readingElements};
 mod reading;
 mod updating;
 mod defining;
 mod iterating;
-mod enumVec;
+mod enum_vec;
 use reading::*;
 fn main() {
     updating::update();
     defining::defining();
     reading::readingElements();
     iterating::iteratingVector();
+    enum_vec::accountsVector();
     // vectors are impl by generics 
     // Vec<T> is generic , T = any type of data
-    let v : Vec<i32> = Vec::new();
-
-    // vec1 macro create a vector of i32 
-    let vv = vec![1,2,3,4];
-
-    enumVec::accountsVector();
 
 }
