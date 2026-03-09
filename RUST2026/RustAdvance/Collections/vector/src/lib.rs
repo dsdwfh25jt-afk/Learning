be06d3e67ca742431f8d9d81c4826a94867c061b
Gaurav <gauravbhat7121@icloud.com>
2026-03-09 11:11:52 +0530

    updated the library

--- RUST2026/RustAdvance/Collections/vector/src/lib.rs
+++ RUST2026/RustAdvance/Collections/vector/src/lib.rs
@@ -1,19 +1,1 @@
-pub mod addition {
-    pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-    }
-}
-
 pub mod reading;
-
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
