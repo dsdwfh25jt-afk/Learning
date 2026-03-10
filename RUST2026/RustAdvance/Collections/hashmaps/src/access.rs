371f20fbe3eff76c749806407ef48e8a15ed01a0
Gaurav <gauravbhat7121@icloud.com>
2026-03-10 23:25:28 +0530

    added iteration

--- RUST2026/RustAdvance/Collections/hashmaps/src/access.rs
+++ RUST2026/RustAdvance/Collections/hashmaps/src/access.rs
@@ -4,17 +4,22 @@ pub fn access_hashmap() {
     // HashMap::new(key,value)
     // created new mutable hashmap called score 
     let mut score = HashMap::new(); 
-
-    score.insert(String::from("yello"),10);
+    
+    score.insert(String::from("pink"),01);
     score.insert(String::from("blue"),11);
-    score.insert(String::from("pink"),01);  
-
-
+    score.insert(String::from("yello"),10);
+    
     // this will get the value value of the key 
     let pink = score.get(&"pink".to_string()).copied().unwrap_or(0);
     // &"pink".to_string() is the key of the HashMap 
     // this get function returns ooption<&T>     
     // by calling .copied() it is returning the option<T> here 
-    println!("{pink:#?}");
-    println!("{score:#?}");
+    println!("{pink}"); // printing value 
+    println!("{score:#?}"); // priting complete hashMap
+
+    // iterating through hashMap
+    // last inserted will print first 
+    for (key , value) in &score {
+        println!("{key} : {value}");
+    }
 }
