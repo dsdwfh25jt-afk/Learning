17faff1ad4dd93ddc1f47919d4494e2c06af98ae
Gaurav <gauravbhat7121@icloud.com>
2026-03-13 11:16:09 +0530

    tested backtrase

--- RUST2026/RustAdvance/Errors/Unrecoverable/panic/src/main.rs
+++ RUST2026/RustAdvance/Errors/Unrecoverable/panic/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let num = 12;
+    let num = 10;
+    // this will couse just panic and tell 
     if num == 12 {
-        panic!("Ruk ja bhes dk");
+        panic!("Stop ..."); 
     }
+
+    let vec = vec![1,2,3];
+    vec[99]; // this will panic and tell index out of bound
+    
     
 }
