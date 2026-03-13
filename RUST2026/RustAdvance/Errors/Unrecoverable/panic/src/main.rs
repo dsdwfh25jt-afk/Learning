d301a14e57cdf0f4f1cc738f9d4e8bb57a2eb20f
Gaurav <gauravbhat7121@icloud.com>
2026-03-13 11:19:40 +0530

    added how to use backtrack

--- RUST2026/RustAdvance/Errors/Unrecoverable/panic/src/main.rs
+++ RUST2026/RustAdvance/Errors/Unrecoverable/panic/src/main.rs
@@ -8,5 +8,11 @@ fn main() {
     let vec = vec![1,2,3];
     vec[99]; // this will panic and tell index out of bound
     
+    // for mac and linux 
+    // RUST_BACKTRACE=1 cargo run 
+
+    // for windows powerShell
+    // $env:RUST_BACKTRACE=1
+    // cargo run
     
 }
