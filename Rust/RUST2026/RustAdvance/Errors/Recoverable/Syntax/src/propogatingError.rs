26c4c05299e5c68c2482019bac964964cb160b0b
Gaurav <gauravbhat7121@icloud.com>
2026-03-17 23:24:38 +0530

    added use of ? operator

--- Rust/RUST2026/RustAdvance/Errors/Recoverable/Syntax/src/propogatingError.rs
+++ Rust/RUST2026/RustAdvance/Errors/Recoverable/Syntax/src/propogatingError.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self,Read};
+use std::fs;
 
 // this function will return the error type to the calling code 
 // which mean if we call this function in main 
@@ -7,9 +8,9 @@ use std::io::{self,Read};
 // according to itself 
 
 pub fn read_username() -> Result<String , io::Error> { // returning results type String (ok) or error 
-    let username_file = File::opne("hello.exe"); 
+    let username_file = File::open("hello.exe"); 
 
-    let usernames = match username_file {
+    let mut usernames = match username_file {
         Ok(file) => file, // file open ok 
         Err(e) => return Err(e), // not open return error 
     }; 
@@ -17,7 +18,26 @@ pub fn read_username() -> Result<String , io::Error> { // returning results type
     let mut user_name = String::new();
 
     match usernames.read_to_string(&mut user_name) {
-        Ok(_) => Ok(username), // if able to read ok
+        Ok(_) => Ok(user_name), // if able to read ok
         Err(e) => Err(e), // if not return error 
     }  // here we not puted ; so it will return the value 
+}
+
+// this function do same the read_username does
+pub fn operator_shortcut() -> Result<String , io::Error>{
+    let mut username_file = File::open("hello.txt")?;
+    let mut user_name = String::new();
+    username_file.read_to_string(&mut user_name)?;
+    Ok(user_name)
+}
+// this function do same the read_username does
+pub fn operator_shortcut1() -> Result<String , io::Error> {
+    let mut user_name = String::new();
+    File::open("hello.txt")?.read_to_string(&mut user_name)?;
+    
+    Ok(user_name)
+}
+
+pub fn build_in_tools() -> Result<String, io::Error> { 
+    fs::read_to_string("hello.txt")
 }
