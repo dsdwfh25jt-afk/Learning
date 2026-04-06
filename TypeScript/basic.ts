39cd8cdf4043a19c3175509cb87467e011ea337b
Gaurav <gauravbhat7121@icloud.com>
2026-04-06 23:55:00 +0530

    added suitable comments for basic.ts

--- TypeScript/basic.ts
+++ TypeScript/basic.ts
@@ -1,5 +1,6 @@
-function greet(  name : string) : string {
-    return `hello ${name}`
+function greet(  name : string) : string { // created fucntion which is taking string in and returns string 
+    return `hello ${name}` // returning the string with appeding the input
 }
-console.log(greet("sol devs"));
-console.log(greet(32));
+console.log(greet("sol devs")); // calling the function we've created with input string 
+// console.log(greet(32)); // well this is not allowed but it will run at the end 
+// good to avoid unwated type conversions 
